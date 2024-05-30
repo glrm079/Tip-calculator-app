c6a5ceb83bd7e1116abbf74b9c754af083f8672f
glrm079 <glrm07928@gmail.com>
2024-05-30 16:32:21 -0300

    input tips almost done

--- index.js
+++ index.js
@@ -2,3 +2,34 @@
 let Value = ()=>{
     return document.getElementById("inputBill").value
 }
+let tipPorcent = ()=>{
+    document.querySelectorAll(".c-select__item").forEach((item)=>{
+        if(item.classList.contains("c-select__item--active")){
+            return item.value
+        }
+    })
+}
+document.querySelectorAll(".c-select__item").forEach((item) => {
+    if(item.classList.contains("unact")){
+
+    }else{
+        item.addEventListener("click", () => {
+            document.querySelectorAll(".c-select__item").forEach((item) => {
+                item.classList.remove("c-select__item--active");
+            });
+            if (item.classList.contains("custom")) {
+                item.addEventListener("click", () => {
+                    item.classList.add("custom--unactive");
+                    document.getElementById("unactiveInput").classList.add("custom--Active");
+                    document.getElementById("unactiveInput").classList.add("c-select__item--active");
+                });
+            } else {
+                item.classList.add("c-select__item--active");
+                document.getElementById("custom").classList.remove("custom--unactive");
+                document.getElementById("unactiveInput").classList.remove("custom--Active");
+                document.getElementById("unactiveInput").classList.add("custom--unactive");
+            }
+        });
+    }
+});
+
