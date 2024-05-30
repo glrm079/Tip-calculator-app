17fe867c3002a783babcf758a71c05081ce48045
glrm079 <glrm07928@gmail.com>
2024-05-30 17:39:42 -0300

    design finished

--- index.js
+++ index.js
@@ -2,13 +2,30 @@
 let Value = ()=>{
     return document.getElementById("inputBill").value
 }
-let tipPorcent = ()=>{
+let TipPorcent = ()=>{
     document.querySelectorAll(".c-select__item").forEach((item)=>{
         if(item.classList.contains("c-select__item--active")){
             return item.value
         }
     })
 }
+let People = ()=>{
+    return document.getElementById("people").value
+}
+
+document.getElementById("reset").addEventListener("click",()=>{
+    let value = Value()
+    let tip = TipPorcent()
+    let people = People()
+    let total = (value / 100)*tip
+    let amount = total/people
+    document.getElementById("total").innerText("$"+ total)
+    document.getElementById("amount").innerText("$"+ amount)
+    alert(total,amount)
+
+})
+
+
 document.querySelectorAll(".c-select__item").forEach((item) => {
     if(item.classList.contains("unact")){
 
